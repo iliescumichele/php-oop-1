a7835abb6512780eea1816bcb62deaf4eb71f0b7
iliescumichele <iliescumichele@gmail.com>
2022-06-25 20:50:29 +0200

    init project

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/*creare un file index.php in cui:
+    - è definita una classe ‘Movie’
+    => all’interno della classe sono dichiarate delle variabili d’istanza
+    => all’interno della classe è definito un costruttore
+    => all’interno della classe è definito almeno un metodo
+
+    - vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
+
+    La classe può essere definita internamente al file index.php o essere inclusa (soluzione preferibile)
+*/
+
+
+?>

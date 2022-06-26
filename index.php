0c438177990ca44fa88da19954f1421ebdd0acc4
iliescumichele <iliescumichele@gmail.com>
2022-06-26 15:25:50 +0200

    Messo in ordine della stampa a video

--- index.php
+++ index.php
@@ -4,9 +4,7 @@
     => all’interno della classe sono dichiarate delle variabili d’istanza
     => all’interno della classe è definito un costruttore
     => all’interno della classe è definito almeno un metodo
-
     - vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
-
     La classe può essere definita internamente al file index.php o essere inclusa (soluzione preferibile)
 */
 
@@ -31,21 +29,6 @@ class movie{
     }
 
     //definisco un metodo
-    public function durationMovie(){
-        $hour = 0;
-        $minutes = $this->duarata;
-
-        $hour = $this->duarata / 60;
-        
-        if($hour > 0){
-            $minutes  = $this->duarata - ($hours * 60);
-            return $this->durata = $hour + ':'  + $minutes;
-        }
-        else{
-            return $this->durata;
-        }
-    }
-
     public function printMovieName(){
         return $this->titolo;
     }    
@@ -56,7 +39,7 @@ $ioSonoLeggenda = new Movie("Io sono Leggenda", 2007, "Horror", 101);
 $spiderman = new Movie("Spiderman", 2014, "Fantasy/Action", 142);
 
 //stampo a video i valori degli oggetti
-echo "nome film: " . $ioSonoLeggenda->printMovieName() . "   Durata: " . $ioSonoLeggenda->durationMovie() . "\n";
-echo "nome film: " . $spiderman->printMovieName() . "   Durata: " . $spiderman->durationMovie() . "\n";
+echo "<br>nome film: " . $ioSonoLeggenda->printMovieName() . "<br>Durata: " . $ioSonoLeggenda->durationMovie();
+echo "<br><br>nome film: " . $spiderman->printMovieName() . "<br>Durata: " . $spiderman->durationMovie();
 
 ?>
